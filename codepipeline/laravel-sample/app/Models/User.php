413eb1d2173c7674ea05cf1935fb9de9928207ae
satoshi256kbyte <kaneyasu2405@gmail.com>
2023-02-25 22:03:07 +0900

    CodeBuildで例外の隠蔽をカウントしてみる

--- codepipeline/laravel-sample/app/Models/User.php
+++ codepipeline/laravel-sample/app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected function _sample()
+    {
+        try {
+            $this->name = 'sample';
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
 }
